3092ac43c7f546e485c7ff077355e4070f98d5f0
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2022-05-28 21:26:41 +0300

    Searchbar working

--- InstAdvisor/instadvisor/advisor/schema.py
+++ InstAdvisor/instadvisor/advisor/schema.py
@@ -1,11 +1,13 @@
 import imp
 from inspect import ismethoddescriptor
+from operator import contains
 from turtle import title
 import graphene
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django import DjangoObjectType
 from .models import Posts
+import django_filters
 from django.db.models import QuerySet
 
 class PostsType(DjangoObjectType):
@@ -19,22 +21,26 @@ class PostsType(DjangoObjectType):
         }
         interfaces = (relay.Node, )
 
+class PostsFilter(django_filters.FilterSet):
+    # Do case-insensitive lookups on 'name'
+    title = django_filters.CharFilter(lookup_expr=['icontains'])
 
+    class Meta:
+        model = Posts
+        fields = ['title', 'id', 'execrpt']
 
 class Query(graphene.ObjectType):
 
 
 
     post = relay.Node.Field(PostsType)
-    all_posts = DjangoFilterConnectionField(PostsType)
-
+    all_posts = DjangoFilterConnectionField(PostsType, filterset_class=PostsFilter)
     #def resolve_all_posts(root, info, **kwargs):
 
     def resolve_all_posts(root, info, search=None):
         queryset = Posts.objects.all()
-        if search:
-            
-            queryset = queryset.filter(filter='icontains', search=search)
+        if search: 
+            queryset = queryset.filter(title__icontains=search)
 
         return queryset
     
